aa6928803d575b76a91b6ab9a5a42072e8e1c853
alkanf <alkanf2000@gmail.com>
2026-07-20 03:30:59 +0300

    feat: finished for loop practices and started while.

---
+++ src/While/PrintNumbersWhile.java
@@ -0,0 +1,19 @@
+package While;
+
+import Loops.PrintNumbers;
+
+public class PrintNumbersWhile {
+    public void printNumbersWhile(int n) {
+    int i = 0; //init
+                while(i<n){  //condition
+                    i++;
+                    System.out.println(i);
+                }
+    }
+
+    public static void main(String[] args) {
+        PrintNumbersWhile obj = new PrintNumbersWhile();
+        obj.printNumbersWhile(5);
+    }
+
+}
