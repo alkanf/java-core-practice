ee0cb30d5c0380cee6d0afdfba5588f76578a305
alkanf <alkanf2000@gmail.com>
2026-07-22 21:46:17 +0300

    feat: added while practice

---
+++ src/While/ReverseNumberWhile.java
@@ -0,0 +1,19 @@
+package While;
+
+public class ReverseNumberWhile {
+    public int reverseNumberWhile(int number) {
+        int reversed = 0;
+while(number>0) {
+    int lastDigit = number % 10; //take last digit from num var
+    reversed = reversed * 10 + lastDigit; //add digit to reversed var
+    number /= 10; // delete last digit from num
+
+}
+return reversed;
+    }
+
+    public static void main(String[] args) {
+        ReverseNumberWhile obj = new ReverseNumberWhile();
+        System.out.println(obj.reverseNumberWhile(1234));
+    }
+}
