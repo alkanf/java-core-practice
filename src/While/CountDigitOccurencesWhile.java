c67d109d1f7bbd01665f0a9ed6abeb8bdfdb3c81
alkanf <alkanf2000@gmail.com>
2026-07-25 01:03:19 +0300

    feat: added new while practices

---
+++ src/While/CountDigitOccurencesWhile.java
@@ -0,0 +1,19 @@
+package While;
+
+public class CountDigitOccurencesWhile {
+    public int countDigitOccurrencesWhile(int number, int target) {
+        int count = 0;
+        while(number>0) {
+            int lastDigit = number % 10;
+            number /= 10;
+            if(lastDigit == target) {count++;}
+        }
+        return count;
+
+    }
+
+    public static void main(String[] args) {
+        CountDigitOccurencesWhile obj = new CountDigitOccurencesWhile();
+        System.out.println(obj.countDigitOccurrencesWhile(434751843, 4));
+    }
+}
